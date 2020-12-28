47ffe224df21fbc5d9a02443abcb08a0cb4862a1
AdulIT <62786359+AdulIT@users.noreply.github.com>
2020-12-28 14:04:20 +0600

    Update ALLOWED_HOSTS with site and development server URL

--- project_1/settings.py
+++ project_1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ngjc)&8a0^!h6@+s9+j@$*=@q=gdkz
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['frozen-depths-58151.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
